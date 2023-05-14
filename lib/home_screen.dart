5511197eee904a8e941bb4021d4e6679abd5b3d4
Ahmed-Reda0 <68137755+Ahmed-Reda-Dev@users.noreply.github.com>
2023-05-14 21:34:14 +0300

    `TFLite-flutter`

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -49,7 +49,7 @@ class _HomeScreenState extends State<HomeScreen> {
   classifyImage(File image) async {
     var output = await Tflite.runModelOnImage(
       path: image.path,
-      numResults: 2,
+      numResults: 10,
       threshold: 0.5,
       imageMean: 127.5,
       imageStd: 127.5,
